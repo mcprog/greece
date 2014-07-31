9306032f4fffd17507f8be601613b12821d6708a
mcprog <michaelcurtis96@gmail.com>
2014-07-31 16:58:09 -0700

    proxy stuff for entities

--- src/main/java/com/mcprog/greece/proxy/CommonProxy.java
+++ src/main/java/com/mcprog/greece/proxy/CommonProxy.java
@@ -1,10 +1,21 @@
 package com.mcprog.greece.proxy;
 
+import com.mcprog.greece.Greece;
+import com.mcprog.greece.entity.EntitySpear;
+import com.mcprog.greece.utility.LogHelper;
+
+import cpw.mods.fml.common.registry.EntityRegistry;
+
 /**
  * 
  * @author mcprog
  *
  */
 public abstract class CommonProxy implements IProxy {
-
+	@Override
+	public void initRenderingAndTextures() {
+//		LogHelper.info("Registering entities");
+//		EntityRegistry.registerModEntity(EntitySpear.class, "entitySpear", 0, Greece.instance, 128, 5, true);
+		
+	}
 }
--- src/main/java/com/mcprog/greece/proxy/ServerProxy.java
+++ src/main/java/com/mcprog/greece/proxy/ServerProxy.java
@@ -1,10 +1,41 @@
 package com.mcprog.greece.proxy;
 
+import com.mcprog.greece.Greece;
+import com.mcprog.greece.entity.EntitySpear;
+import com.mcprog.greece.utility.LogHelper;
+
+import cpw.mods.fml.common.registry.EntityRegistry;
+
 /**
  * 
  * @author mcprog
  *
  */
 public class ServerProxy extends CommonProxy {
+
+	@Override
+	public void registerTileEntities() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initRenderingAndTextures() {
+//		System.err.println("Registering entities");
+//		EntityRegistry.registerModEntity(EntitySpear.class, "entitySpear", 0, Greece.instance, 128, 5, true);
+		
+	}
+
+	@Override
+	public void registerEventHandlers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerKeybindings() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
